e55ca47185960097e1c6dd0aa6bf4a74be54e397
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2024-09-12 11:48:31 -0400

    test commit

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,12 +20,10 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
-//import Categories from '../../_components/Categories'
-//import Promotion from '../../_components/Promotion'
-
-import classes from './index.module.scss'
 import Categories from '../../_components/Categories'
 import Promotion from '../../_components/Promotion'
+
+import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -67,8 +65,8 @@ export default async function Page({ params: { slug = 'home' } }) {
         <section>
           <Hero {...hero} />
 
-         <Gutter className={classes.home}>
-            <Categories categories={categories}/>
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
             <Promotion />
           </Gutter>
         </section>
